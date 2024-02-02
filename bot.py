635c7ee61c3cd4bdef57decec9b4e76cf41c96ae
Aditya Narayan <155406501+ADDI-BOT@users.noreply.github.com>
2024-02-02 21:39:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
+from aiogram import Bot, Dispatcher, types
+from keep_alive import keep_alive
+keep_alive()
 
 
 
